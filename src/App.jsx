b571cb1ea11ee69dab43189a61c251b5ecec5cf3
Yur@Fiv3 <uraporskalo@gmail.com>
2024-12-16 16:10:53 +0200

    Add Button component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Button from './components/Button'
+
 function App() {
   return (
     <div>
-      
+      <h1>App component</h1>
+      <Button text={'Text button'} type={'button'} num={1} disabled={false}/>
+      <Button text={'Submit button'} type={'submit'} num={2} disabled/>
+      <Button text={'Reset button'} type={'reset'} num={3} disabled={false}/>
     </div>
   )
 }
